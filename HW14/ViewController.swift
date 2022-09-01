13077a7be32e6dcdabda67750cb36945701d7c59
viknik08 <63363344+viknik08@users.noreply.github.com>
2022-09-01 21:40:32 +0800

    Initial Commit

---
+++ HW14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW14
+//
+//  Created by Виктор Басиев on 01.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
