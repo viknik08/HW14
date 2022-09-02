a11043b468fab6412315b61a2f26b51f0b2741c6
viknik08 <63363344+viknik08@users.noreply.github.com>
2022-09-02 14:57:09 +0800

    add composition  header cell

---
+++ HW14/Sources/Controllers/CollectionView/HeaderCompositionCell/HeaderCompositionCell.swift
@@ -0,0 +1,22 @@
+//
+//  HeaderCompositionCell.swift
+//  HW14
+//
+//  Created by Виктор Басиев on 02.09.2022.
+//
+
+import UIKit
+
+class HeaderCompositionCell: UICollectionReusableView {
+        
+    static let identifier = "HeaderCompositionCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ HW14/Sources/Controllers/CompositionCell/CompositionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HW14
+//
+//  Created by Виктор Басиев on 01.09.2022.
+//
+
+import UIKit
+
+class CompositionViewCell: UICollectionViewCell {
+
+    static let identifier = "CompositionCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
