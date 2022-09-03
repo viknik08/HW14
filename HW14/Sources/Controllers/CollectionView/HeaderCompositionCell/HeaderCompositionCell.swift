55aa10918a561d690569e7a1395aad42d51c8ca7
viknik08 <63363344+viknik08@users.noreply.github.com>
2022-09-03 13:05:58 +0800

    fill headerView

--- HW14/Sources/Controllers/CollectionView/HeaderCompositionCell/HeaderCompositionCell.swift
+++ HW14/Sources/Controllers/CollectionView/HeaderCompositionCell/HeaderCompositionCell.swift
@@ -10,6 +10,22 @@ import UIKit
 class HeaderCompositionCell: UICollectionReusableView {
         
     static let identifier = "HeaderCompositionCell"
+
+//MARK: - Outlets
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Header"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        return label
+    }()
+    
+    lazy var rightButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("See All", for: .normal)
+        return button
+    }()
+    
+//MARK: - Init
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -19,4 +35,20 @@ class HeaderCompositionCell: UICollectionReusableView {
         fatalError("init(coder:) has not been implemented")
     }
     
+//MARK: - Setup
+    
+    private func setupHierarhy() {
+        addSubview(titleLabel)
+        addSubview(rightButton)
+    }
+    private func setupLayout() {
+        titleLabel.snp.makeConstraints { make in
+            make.left.equalTo(self).offset(10)
+            make.top.equalTo(self).offset(10)
+        }
+        rightButton.snp.makeConstraints { make in
+            make.right.equalTo(self).inset(10)
+            make.top.equalTo(self).offset(10)
+        }
+    }
 }
